f64af33194d4e4ca0f91949201706b4ae8132a05
GerdaAm <63974776+GerdaAm@users.noreply.github.com>
2021-01-01 16:54:27 +0200

    Surname added through concatenation.

--- src/index.js
+++ src/index.js
@@ -2,11 +2,12 @@ import React from "react";
 import ReactDOM from "react-dom";
 
 const name = "Gerda";
+const surname = "Amoraal";
 const luckyNumber = 3;
 
 ReactDOM.render(
   <div>
-    <h1>Hello {name}!</h1>
+    <h1>Hello {name + " " + surname}!</h1>
     <p>Your lucky number is {luckyNumber}.</p>
   </div>,
   document.getElementById("root")
